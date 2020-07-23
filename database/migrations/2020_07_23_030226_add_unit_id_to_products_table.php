3e8749377b42c6ff38cdd4a72ecb729b301adbff
thuyptd <thuyptd@vn.ids.jp>
2020-07-23 10:18:40 +0700

    Update for database

---
+++ database/migrations/2020_07_23_030226_add_unit_id_to_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUnitIdToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+            $table->integer('unit_id')->after('quality')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
